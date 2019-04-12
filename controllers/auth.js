93cdaec2b59f71cd10e8b402afb48f1a9a0a64be
Wendelin Gerein <info@webdelin.de>
2019-04-12 16:42:18 +0200

    marge

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,18 +12,24 @@ module.exports.login = function (req, res) {
 
 module.exports.register = async function (req, res) {
 
-    const candidate = await User.findOne({email: req.body.email})
+
+    const candidate = await User.findOne({where:{ email: req.body.email}})
 
     if(candidate){
         res.status(409).json({
-            message: 'User exist'
+            message: 'Email Exist'
         })
     } else {
+
         return User
-        .create({
-            email: req.body.email,
-            password: req.body.password
-        })
+            .create({
+                email: req.body.email,
+                password: req.body.password
+            })
+            .then((users) => res.status(201).send(users))
+            .catch((error) => res.status(400).send(error));
+
     }
+
 
 }
